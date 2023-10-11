0cb0eac8d7126e069ef3edbd572f8d6dc73306a7
Marianne Shaffer <81380912+shafferma08@users.noreply.github.com>
2023-10-11 07:36:40 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name:
+    File Name: script.js
+    Date: 
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
